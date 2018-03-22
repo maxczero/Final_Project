67f70c2edb184dd9a876e3860f37eacb4903ffa9
Max Chidzero <mwc349@nyu.edu>
2018-03-22 13:46:52 -0400

    added digital write

--- Final_Project.ino
+++ Final_Project.ino
@@ -2,11 +2,10 @@ int ledPin = 13;
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
 
 }
