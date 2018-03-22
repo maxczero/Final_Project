945c0ca0840bc17c6ffe77edfe47719f18b3193e
Max Chidzero <mwc349@nyu.edu>
2018-03-22 13:49:03 -0400

    two led's

--- Final_Project.ino
+++ Final_Project.ino
@@ -1,11 +1,12 @@
 int ledPin = 13;
-
+int ledPin2 = 14;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
-
+  digitalWrite(ledPin2, HIGH);
 }
